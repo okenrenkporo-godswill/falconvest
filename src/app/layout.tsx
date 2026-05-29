9d77d3c3fbf3e052b387889db8b3a9e9baefbcb9
gammaf <okenrenkporooritsewinor@gmail.com>
2026-05-30 00:07:32 +0100

    feat: Add Smartsupp Live Chat integration with custom brand color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Providers } from "@/components/providers";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,7 +31,21 @@ export default async function RootLayout({
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Providers>{children}</Providers>
         </NextIntlClientProvider>
+        <Script id="smartsupp-chat" strategy="afterInteractive">
+          {`
+            window._smartsupp = window._smartsupp || {};
+            window._smartsupp.key = '58024e205dccbafcff726e8499b5bcb1f41c87d7';
+            window._smartsupp.color = '#33525c';
+            window.smartsupp||(function(d) {
+              var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+              s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+              c.type='text/javascript';c.charset='utf-8';c.async=true;
+              c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+            })(document);
+          `}
+        </Script>
       </body>
     </html>
   );
 }
+
